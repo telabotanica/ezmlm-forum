4383e2adbca6730be424c6de8034aeccd8ee42b0
Mathias Chouet <mathias@tela-botanica.org>
2017-07-07 14:51:57 +0200

    If no avatar was found, generates one using author's initials

--- js/EzmlmForum.js
+++ js/EzmlmForum.js
@@ -354,3 +354,17 @@ EzmlmForum.prototype.loadUserInfo = function(cb) {
 		cb(); // the token seems invalid; load app anyway
 	}
 };
+
+
+// simple hash function converting a string to an integer
+// http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+String.prototype.hashCode = function(){
+	var hash = 0;
+	if (this.length == 0) return hash;
+	for (i = 0; i < this.length; i++) {
+		char = this.charCodeAt(i);
+		hash = ((hash<<5)-hash)+char;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return hash;
+};
